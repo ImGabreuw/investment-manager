9bffbc831a5ce082a8872c422654572ee117755a
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-17 17:34:27 -0300

    feat(FIIs): get patrimony unformatted

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { launch, Page } from "puppeteer";
 
-async function getCurrentValue(page: Page): Promise<string | null> {
-  await page.waitForXPath(
-    `/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`
-  );
+async function getRawCurrentValue(page: Page): Promise<string | null> {
+  const xpath = `/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`;
 
-  const currentValueElementHandle = await page.$x(
-    `/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`
-  );
+  await page.waitForXPath(xpath);
+
+  const currentValueElementHandle = await page.$x(xpath);
 
   const currentValue = await page.evaluate(
     (currentValueTag) => currentValueTag.textContent,
@@ -17,13 +15,30 @@ async function getCurrentValue(page: Page): Promise<string | null> {
   return currentValue;
 }
 
+async function getRawPatrimony(page: Page): Promise<string | null> {
+  const xpath = `/html/body/main/div[2]/div[5]/div/div[1]/div/div[2]/span[2]`;
+
+  await page.waitForXPath(xpath);
+
+  const patrimonyElementHandle = await page.$x(xpath);
+
+  const patrimony = await page.evaluate(
+    (patrimonyTag) => patrimonyTag.textContent,
+    patrimonyElementHandle[0]
+  );
+
+  return patrimony;
+}
+
 const browser = await launch({ headless: false });
 const page = await browser.newPage();
 
 await page.goto("https://statusinvest.com.br/fundos-imobiliarios/mxrf11");
 
-const currentValue = await getCurrentValue(page);
-
+const currentValue = await getRawCurrentValue(page);
 console.log(currentValue);
+
+const patrimony = await getRawPatrimony(page);
+console.log(patrimony);
 
 await browser.close();
