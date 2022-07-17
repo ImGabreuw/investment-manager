0645e5807f1db71ff8fcd5d74fae8e806ce48ca1
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-17 17:16:51 -0300

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { launch } from "puppeteer";
+
+(async () => {
+  const browser = await launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto("https://statusinvest.com.br/fundos-imobiliarios/mxrf11");
+
+
+  await page.waitForXPath(`/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`);
+
+  const currentValueElementHandle = await page.$x(`/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`);
+
+  const currentValue = await page.evaluate(currentValueTag => currentValueTag.textContent, currentValueElementHandle[0]);
+
+  console.log(currentValue);
+
+  await browser.close();
+})();
