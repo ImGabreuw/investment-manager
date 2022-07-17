74abd2e2898e1a1225ed5a490850bbd9732335f9
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-17 18:36:47 -0300

    refactor(FIIs): create function to extract values

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
 import { launch, Page } from "puppeteer";
 
-async function getRawCurrentValue(page: Page): Promise<string | null> {
-  const xpath = `/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`;
-
+async function extractFrom(page: Page, xpath: string): Promise<string | null> {
   await page.waitForXPath(xpath);
 
-  const currentValueElementHandle = await page.$x(xpath);
+  const elementHandle = await page.$x(xpath);
 
-  const currentValue = await page.evaluate(
-    (currentValueTag) => currentValueTag.textContent,
-    currentValueElementHandle[0]
-  );
+  const value = await page.evaluate((tag) => tag.textContent, elementHandle[0]);
 
-  return currentValue;
+  return value;
 }
 
-async function getRawPatrimony(page: Page): Promise<string | null> {
+async function getCurrentValue(page: Page): Promise<number | null> {
+  const xpath = `/html/body/main/div[2]/div[1]/div[1]/div/div[1]/strong`;
+
+  const rawCurrentValue = await extractFrom(page, xpath);
+
+  if (!rawCurrentValue) {
+    console.warn(`Não foi possível extrair "VALOR ATUAL" de ${page.url()}`);
+    return null;
+  }
+
+  const currentValue = rawCurrentValue.replace(",", ".");
+
+  return parseFloat(currentValue);
+}
+
+async function getPatrimony(page: Page): Promise<number | null> {
   const xpath = `/html/body/main/div[2]/div[5]/div/div[1]/div/div[2]/span[2]`;
 
-  await page.waitForXPath(xpath);
+  const rawPatrimony = await extractFrom(page, xpath);
 
-  const patrimonyElementHandle = await page.$x(xpath);
+  if (!rawPatrimony) {
+    console.warn(`Não foi possível extrair "PATRIMÔNIO" de ${page.url()}`);
+    return null;
+  }
 
-  const patrimony = await page.evaluate(
-    (patrimonyTag) => patrimonyTag.textContent,
-    patrimonyElementHandle[0]
-  );
+  const patrimony = rawPatrimony
+    .split(" ")
+    .pop()
+    ?.replaceAll(".", "") as string;
 
-  return patrimony;
+  return parseFloat(patrimony);
 }
 
 const browser = await launch({ headless: false });
@@ -35,10 +48,12 @@ const page = await browser.newPage();
 
 await page.goto("https://statusinvest.com.br/fundos-imobiliarios/mxrf11");
 
-const currentValue = await getRawCurrentValue(page);
+const currentValue = await getCurrentValue(page);
 console.log(currentValue);
+console.log(typeof currentValue);
 
-const patrimony = await getRawPatrimony(page);
+const patrimony = await getPatrimony(page);
 console.log(patrimony);
+console.log(typeof patrimony);
 
 await browser.close();
