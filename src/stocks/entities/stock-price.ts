f01f41f15a4e0c65d07ed11fc0a3a1f3e17692d5
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-21 18:12:47 -0300

    feat(stock): add sotck price entity

---
+++ src/stocks/entities/stock-price.ts
@@ -0,0 +1,33 @@
+import { CurrencyHelper } from "../helpers/currency-helper";
+
+class StockPrice {
+  constructor(
+    readonly current: number | null,
+    readonly minIn1Year: number | null,
+    readonly minInCurrentMonth: number | null,
+    readonly maxIn1Year: number | null,
+    readonly maxInCurrentMonth: number | null
+  ) {}
+
+  getCurrentFormatted(): string {
+    return CurrencyHelper.format(this.current);
+  }
+
+  getMinIn1YearFormatted(): string {
+    return CurrencyHelper.format(this.minIn1Year);
+  }
+
+  getMinInCurrentMonthFormatted(): string {
+    return CurrencyHelper.format(this.minInCurrentMonth);
+  }
+
+  getMaxIn1YearFormatted(): string {
+    return CurrencyHelper.format(this.maxIn1Year);
+  }
+
+  getMaxInCurrentMonthFormatted(): string {
+    return CurrencyHelper.format(this.maxInCurrentMonth);
+  }
+}
+
+export { StockPrice };
