885ca12b9c6d976b6d40dcfdf01fbde1826f1485
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-24 17:35:31 -0300

    feat(stock): add xpath of stock indicators

--- src/stocks/api/dto/status-invest-stock-dto.ts
+++ src/stocks/api/dto/status-invest-stock-dto.ts
@@ -1,6 +1,67 @@
 class StatusInvestStockDTO {
-  readonly "VALOR ATUAL": number
-  readonly "MIN. 52 SEMANAS": number
+  /*
+  Resumo
+  */
+  readonly "VALOR ATUAL": number;
+  readonly "MIN. 52 SEMANAS": number;
+  readonly "MIN. MÊS": number;
+  readonly "MÁX. 52 SEMANAS": number;
+  readonly "MÁX. MÊS": number;
+  readonly "DIVIDEND YIELD": number;
+  readonly "ÚLTIMOS 12 MESES": number;
+  readonly "VALORIZAÇÃO (12M)": number;
+  readonly "MÊS ATUAL": number;
+  /*
+  Indicadores de negociação
+  */
+  readonly "TIPO AÇÃO": string;
+  readonly "TAG ALONG": number;
+  readonly "LIQ. MÉD. DIÁRIA": number;
+  readonly "PARTICIPAÇÃO NO IBOV": number;
+  /*
+  Indicadores de valuation
+  */
+  readonly "P/L": number;
+  readonly "PEG Ratio": number;
+  readonly "P/VP": number;
+  readonly "EV/EBITDA": number;
+  readonly "EV/EBIT": number;
+  readonly "P/EBITDA": number;
+  readonly "P/EBIT": number;
+  readonly "VPA": number;
+  readonly "P/Ativo": number;
+  readonly "LPA": number;
+  readonly "P/SR": number;
+  readonly "P/Cap. Giro": number;
+  readonly "P/Ativo Circ. Liq.": number;
+  /*
+  Indicadores de endividamento
+  */
+  readonly "Dív. líquida/PL": number;
+  readonly "Dív. líquida/EBITDA": number;
+  readonly "Dív. líquida/EBIT": number;
+  readonly "PL/Ativos": number;
+  readonly "Passivos/Ativos": number;
+  readonly "Liq. corrente": number;
+  /*
+  Indicadores de eficiência
+  */
+  readonly "M. Bruta": number;
+  readonly "M. EBITDA": number;
+  readonly "M. EBIT": number;
+  readonly "M. Líquida": number;
+  /*
+  Indicadores de rentabilidade
+  */
+  readonly "ROE": number;
+  readonly "ROA": number;
+  readonly "ROIC": number;
+  readonly "Giro ativos": number;
+  /*
+  Indicadores de crescimento
+  */
+  readonly "CAGR Receitas 5 anos": number;
+  readonly "CAGR Lucros 5 anos": number;
 
   constructor() {}
 }
--- src/stocks/api/status-invest-stock-api.ts
+++ src/stocks/api/status-invest-stock-api.ts
@@ -1,8 +1,13 @@
 import { Page } from "puppeteer";
+import { CurrencyHelper } from "../helpers/currency-helper.js";
+import { NumberHelper } from "../helpers/number-helper.js";
 import { PuppeteerHelper } from "../helpers/puppeteer-helper.js";
 import { StatusInvestStockDTO } from "./dto/status-invest-stock-dto.js";
 
 const FULL_XPATH = {
+  /*
+  Resumo
+  */
   "VALOR ATUAL": `/html/body/main/div[2]/div/div[1]/div/div[1]/div/div[1]/strong`,
   "MIN. 52 SEMANAS": `/html/body/main/div[2]/div/div[1]/div/div[2]/div/div[1]/strong`,
   "MIN. MÊS": `/html/body/main/div[2]/div/div[1]/div/div[2]/div/div[2]/div/span[2]`,
@@ -12,10 +17,57 @@ const FULL_XPATH = {
   "ÚLTIMOS 12 MESES": `/html/body/main/div[2]/div/div[1]/div/div[4]/div/div[2]/div/span[2]`,
   "VALORIZAÇÃO (12M)": `/html/body/main/div[2]/div/div[1]/div/div[5]/div/div[1]/strong`,
   "MÊS ATUAL": `/html/body/main/div[2]/div/div[1]/div/div[5]/div/div[2]/div/span[2]/b`,
+  /*
+  Indicadores de negociação
+  */
   "TIPO AÇÃO": `/html/body/main/div[2]/div/div[5]/div/div/div[1]/div/div/h3/strong`,
   "TAG ALONG": `/html/body/main/div[2]/div/div[5]/div/div/div[2]/div/div/div/strong`,
   "LIQ. MÉD. DIÁRIA": `/html/body/main/div[2]/div/div[5]/div/div/div[3]/div/div/div/strong`,
   "PARTICIPAÇÃO NO IBOV": `/html/body/main/div[2]/div/div[5]/div/div/div[4]/div/a/div/div/strong`,
+  /*
+  Indicadores de valuation
+  */
+  "P/L": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[2]/div/div/strong`,
+  "PEG Ratio": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[3]/div/div/strong`,
+  "P/VP": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[4]/div/div/strong`,
+  "EV/EBITDA": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[5]/div/div/strong`,
+  "EV/EBIT": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[6]/div/div/strong`,
+  "P/EBITDA": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[7]/div/div/strong`,
+  "P/EBIT": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[8]/div/div/strong`,
+  VPA: `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[9]/div/div/strong`,
+  "P/Ativo": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[10]/div/div/strong`,
+  LPA: `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[11]/div/div/strong`,
+  "P/SR": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[12]/div/div/strong`,
+  "P/Cap. Giro": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[13]/div/div/strong`,
+  "P/Ativo Circ. Liq.": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[1]/div/div[14]/div/div/strong`,
+  /*
+  Indicadores de endividamento
+  */
+  "Dív. líquida/PL": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[1]/div/div/strong`,
+  "Dív. líquida/EBITDA": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[2]/div/div/strong`,
+  "Dív. líquida/EBIT": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[3]/div/div/strong`,
+  "PL/Ativos": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[4]/div/div/strong`,
+  "Passivos/Ativos": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[5]/div/div/strong`,
+  "Liq. corrente": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[2]/div/div[6]/div/div/strong`,
+  /*
+  Indicadores de eficiência
+  */
+  "M. Bruta": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[3]/div/div[1]/div/div/strong`,
+  "M. EBITDA": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[3]/div/div[2]/div/div/strong`,
+  "M. EBIT": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[3]/div/div[3]/div/div/strong`,
+  "M. Líquida": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[3]/div/div[4]/div/div/strong`,
+  /*
+  Indicadores de rentabilidade
+  */
+  ROE: `/html/body/main/div[2]/div/div[7]/div[2]/div/div[4]/div/div[1]/div/div/strong`,
+  ROA: `/html/body/main/div[2]/div/div[7]/div[2]/div/div[4]/div/div[2]/div/div/strong`,
+  ROIC: `/html/body/main/div[2]/div/div[7]/div[2]/div/div[4]/div/div[3]/div/div/strong`,
+  "Giro ativos": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[4]/div/div[4]/div/div/strong`,
+  /*
+  Indicadores de crescimento
+  */
+  "CAGR Receitas 5 anos": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[5]/div/div[1]/div/div/strong`,
+  "CAGR Lucros 5 anos": `/html/body/main/div[2]/div/div[7]/div[2]/div/div[5]/div/div[2]/div/div/strong`,
 };
 
 const BASE_URL = "https://statusinvest.com.br/acoes";
@@ -28,18 +80,36 @@ class StatusInvestStockAPI {
   }
 
   async extractData(): Promise<StatusInvestStockDTO> {
-    const dto = Object.create(StatusInvestStockDTO);
+    const dto = Object.create(new StatusInvestStockDTO());
 
     for (const indicatorName of Object.getOwnPropertyNames(FULL_XPATH)) {
       const indicatorXPATH = eval(`FULL_XPATH["${indicatorName}"]`);
 
-      dto[indicatorName] = await PuppeteerHelper.extractTextFrom(
+      const indicator = await PuppeteerHelper.extractTextFrom(
         this.page,
         indicatorXPATH
       );
+
+      dto[indicatorName] = this.normalizeAndConvert(indicator);
     }
 
     return dto;
+  }
+
+  private normalizeAndConvert(text: string | null): string | number {
+    if (text === null) {
+      return "-/-";
+    }
+
+    text = CurrencyHelper.removeBrazilianCurrencySymbol(text);
+    text = NumberHelper.removePercentSymbol(text);
+    text = NumberHelper.removeNumberFormat(text);
+
+    if (NumberHelper.isNotNumber(text)) {
+      return text.trim();
+    }
+
+    return Number(Number(text).toFixed(3));
   }
 }
 
