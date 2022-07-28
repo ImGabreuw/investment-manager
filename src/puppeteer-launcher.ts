a6dc73cb1f3cc4e876e761bf0122e05d12f14423
ImGabreuw <imgabreuw.dev@gmail.com>
2022-07-28 20:13:30 -0300

    fix(stock): implement new Status Invest extraction process logic

--- src/puppeteer-launcher.ts
+++ src/puppeteer-launcher.ts
@@ -5,17 +5,18 @@ import { ReclameAquiAPI } from "./reclame-aqui/api/reclame-aqui-api.js";
 import { ReclameAquiStepsService } from "./reclame-aqui/service/reclame-aqui-steps-service.js";
 import { ReclameAquiXPathService } from "./reclame-aqui/service/reclame-aqui-xpath-service.js";
 import { StatusInvest } from "./status-invest.js";
+import { StatusInvestStockDTO } from "./stocks/api/dto/status-invest-stock-dto.js";
 import { StatusInvestStockAPI } from "./stocks/api/status-invest-stock-api.js";
-import { StatusInvestMapper } from "./stocks/config/mapper/status-invest-mapper.js";
-import { StepsService } from "./stocks/service/steps-service.js";
+import { StatusInvestStepsService } from "./stocks/service/status-invest-steps-service.js";
+import { StatusInvestXPathService } from "./stocks/service/status-invest-xpath-service.js";
 
 const browser = await launch({ headless: false });
 const page = await browser.newPage();
 page.setViewport({ width: 1200, height: 1000 });
 
 // await realStateFundTest();
-// await stockTest();
-await reclameAquiTest();
+await stockTest();
+// await reclameAquiTest();
 
 await browser.close();
 
@@ -29,17 +30,24 @@ async function realStateFundTest() {
 }
 
 async function stockTest() {
-  const stepsService = new StepsService(page);
-  const statusInvest = new StatusInvestStockAPI(page, stepsService);
+  const statusInvestStepsService = new StatusInvestStepsService(page);
+  const statusInvestXPathService = new StatusInvestXPathService();
+  const statusInvestStockAPI = new StatusInvestStockAPI(
+    page,
+    statusInvestStepsService,
+    statusInvestXPathService
+  );
 
-  const dto = await statusInvest.search("TAEE11");
+  const result = await statusInvestStockAPI.search<StatusInvestStockDTO>(
+    "taee11"
+  );
 
-  console.log(await StatusInvestMapper.map(dto));
+  console.log(result);
 }
 
 async function reclameAquiTest() {
-  const reclameAquiXPathService = new ReclameAquiXPathService();
   const reclameAquiStepsService = new ReclameAquiStepsService(page);
+  const reclameAquiXPathService = new ReclameAquiXPathService();
   const reclameAquiAPI = new ReclameAquiAPI(
     page,
     reclameAquiStepsService,
